420d9c953e7006892a2e4662c221f8ed3c6617ce
lufz <lucasfrazzetta@gmail.com>
2023-08-20 14:15:44 -0300

    rutas y servicios

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
+const express = require('express');
 const routerProducts = require('./products.routes');
 const routerCategories = require('./categories.routes');
 const routerUsers = require('./users.routes');
 
 function routerApi(app) {
-  app.use('/products', routerProducts);
-  app.use('/categories', routerCategories);
-  app.use('/users', routerUsers);
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', routerProducts);
+  router.use('/categories', routerCategories);
+  router.use('/users', routerUsers);
 }
 
 module.exports = routerApi;
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../services/product.service');
+
+const service = new ProductsService();
 
 const routerProducts = express.Router();
 routerProducts.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10; // si no viene size, asigna 10
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
+  const products = service.find();
   res.status(200).json(products);
 });
 
@@ -24,10 +18,49 @@ routerProducts.get('/filter', (req, res) => {
 
 routerProducts.get('/:pid', (req, res) => {
   const { pid } = req.params;
-  res.json({
+  const product = service.findOne(pid);
+  if (product) {
+    res.status(200).json(product);
+  } else {
+    res.status(404).json({
+      message: 'not found',
+    });
+  }
+});
+
+routerProducts.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body,
+  });
+});
+
+routerProducts.put('/:pid', (req, res) => {
+  const { pid } = req.params;
+  const body = req.body;
+  res.status(200).json({
+    message: 'update',
+    data: body,
     id: pid,
-    name: 'Product',
-    proce: '1520',
+  });
+});
+
+routerProducts.patch('/:pid', (req, res) => {
+  const { pid } = req.params;
+  const body = req.body;
+  res.status(200).json({
+    message: 'update',
+    data: body,
+    id: pid,
+  });
+});
+
+routerProducts.delete('/:pid', (req, res) => {
+  const { pid } = req.params;
+  res.status(200).json({
+    message: 'delete',
+    id: pid,
   });
 });
 
